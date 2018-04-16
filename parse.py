eaa92e2a3841e65faed479b6c187b5763377b6fa
Diego Toledo <toledo.diego09@gmail.com>
2018-04-15 22:11:30 -0500

    inserts one file into the database

--- parse.py
+++ parse.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 import MySQLdb
-file=open("/Users/lrocg/Desktop/104.", "r")
+file=open("/Users/toledo/Desktop/maildir/skilling-j/_sent_mail/1.", "r")
 
+# Open database connection
+db = MySQLdb.connect(host="enron.c9hkwqr7bv7z.us-east-1.rds.amazonaws.com",port=3306,user="enron_tagger",passwd="CompilersProject",db="enron_tags" )
 
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+#cursor.execute("SELECT VERSION()")
 
 content=file.readline()
 
@@ -33,3 +40,12 @@ while content:
 content=file.read()
 body=content.split("\n\n\n")[0]
 print(body)
+
+#insert into database
+query="INSERT INTO enron (Date,FromC,ToC,XFrom,XTo,Subject,Body) Values ('"+date+"','"+fr+"','"+to+"','"+Xfr+"','"+Xto+"','"+subject+"','"+body+"')"
+print(query)
+cursor.execute(query)
+db.commit()
+
+#close database
+db.close()
