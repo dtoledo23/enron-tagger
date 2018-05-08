4388b14e5438aea0a0ddd3055762a86869b91151
Diego Toledo <toledo@Diegos-MacBook-Pro.local>
2018-05-08 12:20:02 -0500

    Fix email parsers: - Use postgres - Insert only body and date - Limit the search to a few handles

--- parse.py
+++ parse.py
@@ -2,67 +2,64 @@
 
 import re
 import os
-import MySQLdb
+import psycopg2
+# import MySQLdb
 
 # Open database connection
-db = MySQLdb.connect(host="enron-corpora.c9hkwqr7bv7z.us-east-1.rds.amazonaws.com",port=51153,user="whitegirl",passwd="CompilersProject",db="tags" )
+
+conn = psycopg2.connect(host="localhost",database="enron_corpora", user="postgres")
 
 # prepare a cursor object using cursor() method
-cursor = db.cursor()
+cursor = conn.cursor()
 
 # execute SQL query using execute() method.
 #cursor.execute("SELECT VERSION()")
 
 def readEmailFromDirectory(path):
 
-    file=open(path,"r")
+    try:
+        file=open(path,"r")
 
-    content=file.readline()
-
-    while content:
-        if content.startswith("Date:") :
-            date=content.replace("Date:","")
-            print(date)
-        elif content.startswith("From:") :
-            fr=content.replace("From:","")
-            print(fr)
-        elif content.startswith("To:") :
-            to=content.replace("To:","")
-            print(to)
-        elif content.startswith("X-From:") :
-            Xfr=content.replace("X-From:","")
-            print(Xfr)
-        elif content.startswith("X-To:") :
-            Xto=content.replace("X-To:","")
-            print(Xto)
-        elif content.startswith("Subject:") :
-            subject=content.replace("Subject:","")
-            print(subject)
-        elif content.startswith("X-FileName:") :
-            break
         content=file.readline()
 
-    content=file.read()
-    body=content.split("\n\n\n")[0]
-    print(body)
+        while content:
+            if content.startswith("Date:") :
+                date=content.replace("Date:","")
+            elif content.startswith("From:") :
+                fr=content.replace("From:","")
+            elif content.startswith("To:") :
+                to=content.replace("To:","")
+            elif content.startswith("X-From:") :
+                Xfr=content.replace("X-From:","")
+            elif content.startswith("X-To:") :
+                Xto=content.replace("X-To:","")
+            elif content.startswith("Subject:") :
+                subject=content.replace("Subject:","")
+            elif content.startswith("X-FileName:") :
+                break
+            content=file.readline()
 
-    #insert into database
-    # query="INSERT INTO enron (Date,FromC,ToC,XFrom,XTo,Subject,Body) Values ('"+date+"','"+fr+"','"+to+"','"+Xfr+"','"+Xto+"','"+subject+"','"+body+"')"
+        content=file.read()
+        body=content.split("\n\n\n")[0].strip()
 
-    query="INSERT INTO enron (Date,FromC,ToC,XFrom,XTo,Subject,Body) Values (%s,%s,%s,%s,%s,%s,%s)"
-    # print(query)
-    cursor.execute(query, (date, fr, to, Xfr, Xto, subject, body))
-    db.commit()
+        query="INSERT INTO email_tags (datetime, body) VALUES (to_timestamp(%s, 'DY, DD Mon YYYY HH24:MI:SS'),%s)"
+        cursor.execute(query, (date, body))
+        conn.commit()
+    except:
+        print("An error happened with", path)
 
 
 myPath="/Users/toledo/Desktop/maildir"
+handles = [ "lay-k", "skilling-j", "fastow-a", "whalley-l", "whalley-g", "sanders-r", "symes-k", "germany-c", "white-s", "presto-k"]
 files=[]
 
-for path, dirs, files in os.walk(myPath):
-    for filename in files:
-        fullpath = os.path.join(path, filename)
-        print(fullpath)
-        readEmailFromDirectory(fullpath)
+for handle in handles:
+    path = myPath + "/" + handle
+    print("Processing ", handle)
+    for path, dirs, files in os.walk(path):
+        for filename in files:
+            fullpath = os.path.join(path, filename)
+            readEmailFromDirectory(fullpath)
 
 #close database
-db.close()
+conn.close()
