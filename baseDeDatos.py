6ee880c64166df3960c2afca212110e99bdaac15
Rodolfo Lepe <rudylepe5@gmail.com>
2018-05-07 22:33:02 -0500

    Classifier casi funcionando

--- baseDeDatos.py
+++ baseDeDatos.py
@@ -1,28 +1,62 @@
-import MySQLdb
-#file=open("/Users/toledo/Desktop/maildir/skilling-j/_sent_mail/1.", "r")
+import psycopg2
+import nltk
+from nltk.tokenize import word_tokenize # or use some other tokenizer
 
-# Open database connection
-db = MySQLdb.connect(host="enron.c9hkwqr7bv7z.us-east-1.rds.amazonaws.com",port=3306,user="enron_tagger",passwd="CompilersProject",db="enron_tags" )
-
-# prepare a cursor object using cursor() method
+db = psycopg2.connect(host="10.43.103.33", port="5432",database="enron_corpora", user="postgres")
 cursor = db.cursor()
-sql = "SELECT * FROM enron WHERE tag IS NOT NULL"
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   for row in results:
-      date = row[1]
-      body = row[7]
-      tag = row[8]
 
-      # Now print fetched result
-      print ("Date =" + date + "\nBody = " + body + "\nTag = " + tag)
-except:
-   print ("Error: unable to fecth data")
+def searchCategories():
 
-# disconnect from server
-db.close()
+      sql = "SELECT body,tag FROM email_tags WHERE tag IS NOT NULL"
+      try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Fetch all the rows in a list of lists.
+            return cursor.fetchall()
+
+      except:
+            print ("Error: unable to fecth data")
+
 
 #print("Papaya de celaya")
+
+train = searchCategories()
+print(train)
+
+
+def searchWithoutCategories():
+
+      sql = "SELECT body, datetime FROM email_tags WHERE tag IS NULL order by id limit 10"
+      try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Fetch all the rows in a list of lists.
+            return cursor.fetchall()
+
+                 
+      except:
+            print ("Error: unable to fecth data")
+
+
+
+
+test = searchWithoutCategories()
+
+
+all_words = set(word.lower() for passage in train for word in word_tokenize(passage[0]))
+t = [({word: (word in word_tokenize(x[0])) for word in all_words}, x[1]) for x in train]
+classifier = nltk.NaiveBayesClassifier.train(t)
+classifier.show_most_informative_features()
+
+
+def applyClassifier():
+      for row in test:
+            date = row[1]
+            test_sentence = row[0]
+            test_sent_features = {word.lower(): (word in word_tokenize(test_sentence.lower())) for word in all_words}
+            print(classifier.classify(test_sent_features))
+            print(date)
+
+
+applyClassifier()
+db.close()
